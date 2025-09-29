c4c2dfec2dba8f056c9a885c0b645ebd793fdadb
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2025-09-29 17:52:43 +0530

    Update handler.js

--- serverless_framework/handler.js
+++ serverless_framework/handler.js
@@ -1,6 +1,48 @@
-const axios = require('axios')
+import axios from 'axios'
+import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
+
+
+const sesClient = new SESClient();
+
 
 module.exports.alert = async(event) => {
     console.log('SNS Event Recieved ', event);
-    
+
+    const snsMessage = event.Records[0].Sns.Message
+    const webhookURL = process.env.WEBHOOK_URL
+
+    const slackMessage = {
+        text: `🚨 AWS Cost Anomaly Detected! 🚨\n> ${snsMessage}`
+    }
+
+    // Send the message to Slack
+    try {
+        await axios.post(webhookURL, slackMessage);
+        console.log('Successfully sent webhook notification.')
+    } catch (error) {
+        console.log('Error sending webhook notification:', error.message)
+    }
+
+    // Send the message to user email using SES
+    const emailParams = {
+        Source: '<sender_email_address>',
+        Destination: {
+            ToAddresses: ['<reciever_email_address>'],
+        },
+        Message: {
+            Subject: { Data: 'AWS Cost Anomaly Detected!' },
+            Body: {
+                Text: { Data: `An AWS Cost Anomaly was detected with the following details:\n\n${snsMessage}` },
+            },
+        }
+    }
+
+    try {
+        await sesClient.send(new SendEmailCommand(emailParams));
+        console.log('Successfully sent email notification.');
+    } catch (error) {
+        console.log('Error Occurred', error.message);
+    }
+
+    return { statusCode: 200, body: 'Notification processed.' }
 }
